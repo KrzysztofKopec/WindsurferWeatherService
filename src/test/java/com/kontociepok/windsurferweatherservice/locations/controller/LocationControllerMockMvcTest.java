7342d5bc0a5dcd5b42bbdf0e3250d6660e455a07
Krzysztof Kopec <kontociepok@gmail.com>
2022-06-02 17:03:19 +0200

    create LocationsCoordinatesRepoTest and LocationControllerMockMvcTest

---
+++ src/test/java/com/kontociepok/windsurferweatherservice/locations/controller/LocationControllerMockMvcTest.java
@@ -0,0 +1,88 @@
+package com.kontociepok.windsurferweatherservice.locations.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kontociepok.windsurferweatherservice.locations.model.LocationCoordinates;
+import com.kontociepok.windsurferweatherservice.locations.repository.LocationCoordinatesRepo;
+import com.kontociepok.windsurferweatherservice.locations.service.LocationService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(LocationController.class)
+class LocationControllerMockMvcTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private LocationCoordinatesRepo locationCoordinatesRepo;
+
+    @MockBean
+    private LocationService locationService;
+
+    @Test
+    void shouldReturnAllCoordinatesWhenExists() throws Exception{
+        //given
+        LocationCoordinatesResponse locationCoordinates = new LocationCoordinatesResponse(1L,12.34,56.78);
+        List<LocationCoordinatesResponse> coordinates = List.of(locationCoordinates);
+        given(locationService.allCoordinates()).willReturn(coordinates);
+
+        // when
+        ResultActions result = mvc.perform(get("/coordinates").contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().is(200))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].lat", is(locationCoordinates.getLat())))
+                .andExpect(jsonPath("$[0].lon", is(locationCoordinates.getLon())));
+
+    }
+    @Test
+    void shouldReturnSaveCoordinates() throws Exception{
+        //given
+        LocationCoordinatesResponse locationCoordinates = new LocationCoordinatesResponse(1L,12.34,56.78);
+        given(locationService.addCoordinate(12.34,56.78)).willReturn(locationCoordinates);
+
+        //when
+        ResultActions result = mvc.perform(post("/coordinates?lat=12.34&lon=56.78").contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        result.andExpect(status().is(200))
+                .andExpect(jsonPath("$.lat").value(12.34))
+                .andExpect(jsonPath("$.lon").value(56.78));
+    }
+    @Test
+    void shouldReturnDeleteCoordinates() throws Exception{
+        //given
+        locationService.addCoordinate(12.34,56.78);
+        locationService.addCoordinate(10D,10D);
+        LocationCoordinatesResponse locationCoordinates = new LocationCoordinatesResponse(1L,12.34,56.78);
+        given(locationService.deleteCoordinates(1L)).willReturn(locationCoordinates);
+
+        //when
+        ResultActions result = mvc.perform(delete("/coordinates?coordinatesId=1"));
+
+        //then
+        result.andExpect(status().is(200))
+                .andExpect(jsonPath("$.lat", is(locationCoordinates.getLat())))
+                .andExpect(jsonPath("$.lon", is(locationCoordinates.getLon())));
+
+    }
+
+}
---
+++ src/test/java/com/kontociepok/windsurferweatherservice/locations/repository/LocationsCoordinatesRepoTest.java
@@ -0,0 +1,84 @@
+package com.kontociepok.windsurferweatherservice.locations.repository;
+
+import com.kontociepok.windsurferweatherservice.locations.model.LocationCoordinates;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase()
+public class LocationsCoordinatesRepoTest {
+
+    @Autowired
+    LocationCoordinatesRepo locationCoordinatesRepo;
+
+    @Test
+    void shouldReturnAllCoordinatesWhenExists(){
+        //given
+        locationCoordinatesRepo.deleteAll();
+        var coordinates = new LocationCoordinates(12.34,56.78);
+        locationCoordinatesRepo.save(coordinates);
+
+        //when
+        var result = locationCoordinatesRepo.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.contains(coordinates)).isTrue();
+    }
+
+    @Test
+    void shouldReturnCoordinatesByIdWhenExists(){
+        //given
+        locationCoordinatesRepo.deleteAll();
+        var coordinates = new LocationCoordinates(12.34,56.78);
+        locationCoordinatesRepo.save(coordinates);
+
+        //when
+        var result = locationCoordinatesRepo.findById(9L);
+
+        //then
+        assertThat(result.isPresent()).isEqualTo(result.get().getLat() == 12.34);
+    }
+
+    @Test
+    void shouldSaveCoordinates(){
+        //given
+        locationCoordinatesRepo.deleteAll();
+        var coordinates = new LocationCoordinates(12.34,56.78);
+
+        //when
+        locationCoordinatesRepo.save(coordinates);
+
+        //then
+        var result = locationCoordinatesRepo.findAll();
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.contains(coordinates)).isTrue();
+
+    }
+
+    @Test
+    void shouldDeletecoordinatesByIdWhenExists(){
+        //given
+        locationCoordinatesRepo.deleteAll();
+        var coordinates = new LocationCoordinates(12.34,56.78);
+        var coordinates1 = new LocationCoordinates(33.11,44.22);
+        locationCoordinatesRepo.save(coordinates);
+        locationCoordinatesRepo.save(coordinates1);
+
+        //when
+        locationCoordinatesRepo.deleteById(6L);
+
+        //then
+        assertThat(locationCoordinatesRepo.findAll().size()).isEqualTo(1);
+
+    }
+}
